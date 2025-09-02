81802584f41c566b168e73d4f1d2c003c741ea4c
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-09-02 11:03:56 +0530

    Update serializers.py

--- machine/serializers.py
+++ machine/serializers.py
@@ -7,22 +7,14 @@ class ObjectIdField(serializers.Field):
     def to_internal_value(self, data):
         return ObjectId(data)
 
-
-from rest_framework import serializers
 from .models import HMSMI
-
 class HMSMISerializer(serializers.ModelSerializer):
-    id = serializers.CharField(read_only=True)  # Override the ObjectId field
     class Meta:
         model = HMSMI
         fields = '__all__'
 
-
-
-from rest_framework import serializers
 from .models import TestResult
-
 class TestResultSerializer(serializers.ModelSerializer):
     class Meta:
-        model = HMSMI
+        model = TestResult
         fields = '__all__'
