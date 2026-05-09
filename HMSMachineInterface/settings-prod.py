cd954086f82ae720ed974ae98a6268c237e7d55c
SMRFT <110091550+SMRFT@users.noreply.github.com>
2026-05-09 14:12:23 +0530

    Update settings-prod.py

--- HMSMachineInterface/settings-prod.py
+++ HMSMachineInterface/settings-prod.py
@@ -57,10 +57,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'HMSMachineInterface.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
